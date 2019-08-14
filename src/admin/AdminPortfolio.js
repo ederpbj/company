19b9a5399d11c50e36b5938eb5c348c0ae90822f
Eder Pires <ederpbj@gmail.com>
2019-08-14 14:11:22 -0300

    28. Criando o formulário de cadastro de portfolio

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -9,8 +9,23 @@ class AdminPortfolio extends Component{
 
     render(){
         return(
-            <div>
-                <h2>Portfolio - área administrativa</h2>
+            <div style={{padding: '120px'}}>
+                <form>
+                    <div className="form-group">
+                        <label for="titulo">Título</label>
+                        <input type="text" className="form-control" id="titulo"  placeholder="Título" />
+                    </div>
+                    <div className="form-group">
+                        <label for="descricao">Descrição</label>
+                        <textarea class="form-control" id="descricao" rows="3"></textarea>
+                    </div>
+                    
+                    <div className="form-group">
+                        <label for="imagem">Imagem</label>
+                        <input type="file" className="form-control-file" id="imagem"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Salvar</button>
+                </form>
             </div>
         )
     }
