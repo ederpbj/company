a7a5ce42f34bc8392e40ac26ca0659fd47dfeff0
Eder Pires <ederpbj@gmail.com>
2019-08-15 16:59:31 -0300

    34. Conectando o form de Login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -3,25 +3,34 @@ import React, {Component} from 'react'
 class Login extends Component {
     constructor(props){
         super(props)
+
+        this.email = null
+        this.senha = null
+
+        this.autenticaUsuario = this.autenticaUsuario.bind(this)
+    }
+
+    autenticaUsuario(){
+        console.log(this.email.value, this.senha.value)
     }
 
     render(){
         return (
-            <div style={{padding: '130px'}}>
+            <div className="container">
                 <h1>Login</h1>
             
                 <form>
                     <div className="form-group">
-                        <label forHtml="email">Email</label>
-                        <input type="email" className="form-control" id="emial" aria-describedby="emailHelp" placeholder="nome@email.com"/>
+                        <label forhtml="email">Email</label>
+                        <input type="email" name="email" ref={ref => this.email = ref} className="form-control" id="emial" aria-describedby="emailHelp" placeholder="nome@email.com"/>
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
-                        <label forHtml="exampleInputPassword1">Senha</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                        <label forhtml="exampleInputPassword1">Senha</label>
+                        <input type="password" name="senha" ref={ref => this.senha = ref} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                     </div>
                     
-                    <button type="submit" className="btn btn-primary">Acessar</button>
+                    <button type="button" className="btn btn-primary" onClick={this.autenticaUsuario}>Acessar</button>
                 </form>
             </div>
         )
