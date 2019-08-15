604a8b5c6051b6f650830bbbf9822101301c1fc0
Eder Pires <ederpbj@gmail.com>
2019-08-15 17:13:05 -0300

    35. Habilitando Autenticação no Firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -23,5 +23,6 @@ var base = Rebase.createClass(app.database());
 //const config = Rebase.createClass(app.database())
 
 export const storage = app.storage()
+export const auth = app.auth()
 
 export default base
