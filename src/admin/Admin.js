1957b9b27fe7f7931e5fbdf387616b55c8ee075e
Eder Pires <ederpbj@gmail.com>
2019-08-16 00:26:54 -0300

    38. Protegendo rota Admin

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 
 import AdminMenu from './AdminMenu';
 import AdminPortfolio from './AdminPortfolio';
+import {auth} from './../firebase-config'
 
 class Admin extends Component{
     constructor(props){
         super(props)
+
+        this.state = {
+            estaLogando: true,
+            estaAutenticado: false,
+            user: null 
+        }
+    }
+
+    componentDidMount(){
+        //verifica se está logado
+        auth.onAuthStateChanged(user => {
+            this.setState({
+                estaLogando: false,
+
+                //(!!) Se tiver um usuário vira true, se não vira false
+                estaAutenticado: !! user,
+                //como é o mesmo nome, se true, armazena usuário
+                user
+            })
+        })
     }
 
     render(){
+        if(this.state.estaLogando){
+            return <p>aguarde ...</p>
+        }
+        if(!this.state.estaAutenticado){
+            return <Redirect to='/login' />
+        }
+
         return(
             <div>
                 <h2>Painel Administrativo</h2>
